4a42bb3e2d9356de09a9715e4362957c8445e8ea
Qian Zhang <qianz424@gmail.com>
2025-02-06 11:40:07 -0600

    Add a header component

---
+++ src/app/base-header/base-header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-base-header',
+  templateUrl: './base-header.component.html',
+  styleUrl: './base-header.component.css'
+})
+export class BaseHeaderComponent {}
