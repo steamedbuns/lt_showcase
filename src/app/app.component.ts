10bf11915c03f0ebc2230d81e6e5a457adcc6295
Qian Zhang <qianz424@gmail.com>
2025-02-06 17:32:04 -0600

    Move flashlight functionality into home component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding } from '@angular/core';
+import { Component } from '@angular/core';
 import { HomeComponent } from "./home/home.component";
 
 @Component({
@@ -7,22 +7,4 @@ import { HomeComponent } from "./home/home.component";
 	styleUrl: './app.component.css',
  	imports: [HomeComponent]
 })
-export class AppComponent {
-	flashlightOn: boolean = false;
-
-	@HostBinding('style.--radius') radius: string = '0px';
-	@HostBinding('style.--mouse-x') xPos: string = '0px';
-	@HostBinding('style.--mouse-y') yPos: string = '0px';
-	@HostBinding('style.--cursor') cursor: string = 'none';
-
-	onMouseMove(event: { clientX: any; clientY: any; }) {
-		this.xPos = `${event.clientX}px`;
-		this.yPos = `${event.clientY}px`;
-	}
-
-	onClick() {
-		this.flashlightOn = !this.flashlightOn;
-		this.radius = this.flashlightOn ? '250px' : '0px';
-		this.cursor = this.flashlightOn ? 'auto' : 'none';
-	}
-}
+export class AppComponent {}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, HostBinding, inject } from '@angular/core';
 import { SecretService } from '../services/secret.service';
 
 @Component({
@@ -8,6 +8,23 @@ import { SecretService } from '../services/secret.service';
 })
 export class HomeComponent {
 	private secretService = inject(SecretService);
+	private flashlightOn: boolean = false;
+
+	@HostBinding('style.--radius') radius: string = '0px';
+	@HostBinding('style.--mouse-x') xPos: string = '0px';
+	@HostBinding('style.--mouse-y') yPos: string = '0px';
+	@HostBinding('style.--cursor') cursor: string = 'none';
+
+	onMouseMove(event: { clientX: any; clientY: any; }) {
+		this.xPos = `${event.clientX}px`;
+		this.yPos = `${event.clientY}px`;
+	}
+
+	onClick() {
+		this.flashlightOn = !this.flashlightOn;
+		this.radius = this.flashlightOn ? '250px' : '0px';
+		this.cursor = this.flashlightOn ? 'auto' : 'none';
+	}
 
 	onInputClick(event: MouseEvent) {
 		event.stopPropagation();
