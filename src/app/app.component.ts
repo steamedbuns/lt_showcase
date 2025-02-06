09b5efde25dff7583d110b71e8d1c81d68a26486
Qian Zhang <qianz424@gmail.com>
2025-02-06 15:49:02 -0600

    Add login form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,6 +17,7 @@ export class AppComponent {
 	@HostBinding('style.--radius') radius: string = '0px';
 	@HostBinding('style.--mouse-x') xPos: string = '0px';
 	@HostBinding('style.--mouse-y') yPos: string = '0px';
+	@HostBinding('style.--cursor') cursor: string = 'none';
 
 	onMouseMove(event: { clientX: any; clientY: any; }) {
 		this.xPos = `${event.clientX}px`;
@@ -26,5 +27,14 @@ export class AppComponent {
 	onClick() {
 		this.flashlightOn = !this.flashlightOn;
 		this.radius = this.flashlightOn ? '250px' : '0px';
+		this.cursor = this.flashlightOn ? 'auto' : 'none';
+	}
+
+	onInputClick(event: MouseEvent) {
+		event.stopPropagation();
+	}
+
+	onButtonClick(event: MouseEvent) {
+		event.stopPropagation();
 	}
 }
