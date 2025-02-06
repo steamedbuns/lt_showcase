adcf4848ceb3398c20e6a9e88b2aeb7bfdc77d86
Qian Zhang <qianz424@gmail.com>
2025-02-06 11:39:31 -0600

    Add type to key

--- src/app/services/secret.service.ts
+++ src/app/services/secret.service.ts
@@ -5,13 +5,14 @@ import { Injectable } from '@angular/core';
 })
 export class SecretService {
 
-	private SECRET_KEY = 'album_api_key';
+	private SECRET_KEY: string = 'album_api_key';
 	constructor() { }
 
 	// Set the api key
 	public setKey(key: string): void {
 		sessionStorage.setItem(this.SECRET_KEY, key);
 	}
+
 	// Get the api key
 	public getKey(): string | null {
 		return sessionStorage.getItem(this.SECRET_KEY);
