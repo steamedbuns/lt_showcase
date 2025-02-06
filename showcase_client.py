449a577554896dd2bcb4e28c5071c7586993dffc
Qian Zhang <qianz424@gmail.com>
2025-02-06 12:05:31 -0600

    Add support for secrets file in python client.

--- showcase_client.py
+++ showcase_client.py
@@ -7,12 +7,25 @@ LT_ALBUMS_URL = 'https://showcase.leantechniques.com/albums'
 
 def main():
 	parser = argparse.ArgumentParser(description='A command line tool to grab showcase albums and output the json results.')
-	parser.add_argument('-k', '--key', action='store', help='API key provided by showcase pdf.')
+	parser.add_argument('-s', '--status-code', action='store_true', help='Flag indicates perform a request and output response code.')
+	parser.add_argument('-k', '--key-file', action='store', required=True, help='File containing the API key provided by showcase pdf.')
 	parser.add_argument('-o', '--output', action='store', help='File to output the results.')
 	args = parser.parse_args()
 
-	headers = {API_KEY_HEADER: args.key}
+	api_key = ''
+	try:
+		with open(args.key_file, 'r') as key_file:
+			api_key = key_file.read().strip()
+	except FileNotFoundError as e:
+		print(e)
+		exit()
+
+	headers = {API_KEY_HEADER: api_key}
 	r = requests.request('GET', LT_ALBUMS_URL, headers=headers)
+
+	print(r.status_code)
+	if args.status_code or r.status_code != 200:
+		exit()
 	response_json = r.json()
 
 	if args.output is not None:
