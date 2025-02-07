b203e2c62e24cfe9ac89046002e48fe56ec7e194
Qian Zhang <qianz424@gmail.com>
2025-02-07 00:24:45 -0600

    Add angular material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { apiKeyInterceptor } from './interceptors/api-key.interceptor';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
 	providers: [
 		provideZoneChangeDetection({ eventCoalescing: true }),
 		provideRouter(routes),
-		provideHttpClient(withInterceptors([apiKeyInterceptor]))
+		provideHttpClient(withInterceptors([apiKeyInterceptor])), provideAnimationsAsync()
 	]
 };
