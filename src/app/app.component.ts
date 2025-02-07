e6a99a7d92793eb6bc7c472c30d1b453e324c3cb
Qian Zhang <qianz424@gmail.com>
2025-02-06 21:24:54 -0600

    Use router outlet to render components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { HomeComponent } from "./home/home.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.css',
- 	imports: [HomeComponent]
+	imports: [RouterOutlet]
 })
 export class AppComponent {}
