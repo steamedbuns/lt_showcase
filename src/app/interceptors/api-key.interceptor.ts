1c5964ed677f68b89308b6da9cd29346eaa5ddce
Qian Zhang <qianz424@gmail.com>
2025-02-06 22:24:35 -0600

    remove url replacement in interceptor

--- src/app/interceptors/api-key.interceptor.ts
+++ src/app/interceptors/api-key.interceptor.ts
@@ -5,7 +5,6 @@ import { SecretService } from '../services/secret.service';
 export const apiKeyInterceptor: HttpInterceptorFn = (req, next) => {
 	const secretService = inject(SecretService);
 	const reqWithSecret = req.clone({
-		url: 'https://showcase.leantechniques.com',
 		headers: req.headers.set('lt_api_key', secretService.getKey())
 	});
 	return next(reqWithSecret);
