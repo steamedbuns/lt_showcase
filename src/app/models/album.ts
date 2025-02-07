f7345c7eb6a7852015f07987a7e6264292f4f0ed
Qian Zhang <qianz424@gmail.com>
2025-02-06 21:25:31 -0600

    Create album and photo models

---
+++ src/app/models/album.ts
@@ -0,0 +1,6 @@
+import { Photo } from "./photo";
+
+export class Album {
+	public albumId: number = -1;
+	public photos: Array<Photo> = [];
+}
---
+++ src/app/models/photo.ts
@@ -0,0 +1,6 @@
+export class Photo {
+	public photoId: number = -1;
+	public url: string = '';
+	public albumId: number = -1;
+	public title: string = '';
+}
