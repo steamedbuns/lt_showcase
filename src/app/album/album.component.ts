a7482d787139f5384c812c3a21be0a8753346038
Qian Zhang <qianz424@gmail.com>
2025-02-07 02:40:23 -0600

    Populate album and album viewer

--- src/app/album/album.component.ts
+++ src/app/album/album.component.ts
@@ -1,28 +1,53 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { Album } from '../models/album';
 import { AlbumApiService } from '../services/album-api.service';
 
 @Component({
   selector: 'app-album',
-  imports: [],
+  imports: [FormsModule, MatFormFieldModule, MatSelectModule, MatInputModule],
   templateUrl: './album.component.html',
-  styleUrl: './album.component.css'
+  styleUrl: './album.component.css',
 })
-export class AlbumComponent {
+export class AlbumComponent implements OnInit {
 	private albumService = inject(AlbumApiService);
 
-	busy = signal(true);
-	albums = signal(new Array<Album>);
+	busy = signal(false);
+	albums = new Array<Album>;
 
-	getAllALbums() {
+	selectedAlbum: Album = this.albums[0];
+	selectionIndex = 0;
+
+	public ngOnInit(): void {
 		this.busy.set(true);
-		this.albumService.getAllAlbums()
-			.subscribe((data) => {
-				if (data !== null)
-					this.albums.set(data);
-				else
-					console.error(`Error: ${this.albumService.lastErrorResponseCode}`);
-				this.busy.set(false);
-			});
+		if (this.albums.length === 0) {
+			this.albumService.getAllAlbums()
+				.subscribe((data) => {
+					if (data !== null) {
+						this.albums = data;
+						this.albums.sort((a, b) => a.albumId - b.albumId);
+						this.albums.forEach(album => {
+							album.photos.sort((a, b) => a.photoId - b.photoId);
+						});
+						this.selectedAlbum = this.albums[0];
+					}
+					else
+						console.error(`Error: ${this.albumService.lastErrorResponseCode}`);
+					this.busy.set(false);
+				});
+		} else {
+			this.busy.set(false);
+		}
+	}
+
+	selectImage(idx: number) {
+		this.selectionIndex = idx;
+	}
+
+	selectChanged() {
+		this.selectionIndex = 0;
 	}
 }
