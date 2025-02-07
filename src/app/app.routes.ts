94aece070397673b58109c3d4c9e06c643d5c4b3
Qian Zhang <qianz424@gmail.com>
2025-02-06 21:26:10 -0600

    Add album component to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: '',
+		pathMatch: 'full',
+		loadComponent: () => {
+			return import('./home/home.component').then((module) => module.HomeComponent)
+		}
+	},
+	{
+		path: 'albums',
+		loadComponent: () => {
+			return import('./album/album.component').then((module) => module.AlbumComponent)
+		}
+	}
+];
